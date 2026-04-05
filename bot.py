dc8b32b7f8a43516ecf45521c75de7dcc71f25d2
satanxavier23-source <satanxavier23@gmail.com>
2026-04-05 16:35:47 +0530

    update bot

--- bot.py
+++ bot.py
@@ -1,56 +1,55 @@
 import telebot
 import yt_dlp
 import os
-import uuid
+import time
 
-BOT_TOKEN = "8623388098:AAGY5lF3kcbAN25K_ZZ5IzzEt0ddcs38RIQ"
+BOT_TOKEN = os.getenv("8623388098:AAGY5lF3kcbAN25K_ZZ5IzzEt0ddcs38RIQ")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "📥 Send Instagram Reel/Post Link")
+    bot.reply_to(
+        message,
+        "Send Instagram Reel/Post/Story or Terabox link 📥"
+    )
+
+
+def download_video(url, chat_id, msg_id):
+
+    def progress_hook(d):
+        if d['status'] == 'downloading':
+            percent = d.get('_percent_str', '0%')
+            try:
+                bot.edit_message_text(
+                    f"⏳ Downloading {percent}",
+                    chat_id,
+                    msg_id
+                )
+            except:
+                pass
+
+    ydl_opts = {
+        'outtmpl': 'video.%(ext)s',
+        'format': 'best',
+        'progress_hooks': [progress_hook],
+        'quiet': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    for file in os.listdir():
+        if file.startswith("video"):
+            return file
 
 
 @bot.message_handler(func=lambda message: True)
-def download_video(message):
-    url = message.text.strip()
+def downloader(message):
 
-    if "instagram.com" not in url:
-        bot.reply_to(message, "❌ Send valid Instagram link")
-        return
-
-    bot.reply_to(message, "⏳ Downloading...")
-
-    file_name = f"video_{uuid.uuid4().hex}.mp4"
-
-    ydl_opts = {
-        'format': 'best',
-        'outtmpl': file_name,
-        'quiet': True,
-        'noplaylist': True,
-        'nocheckcertificate': True,
-        'ignoreerrors': False
-    }
+    url = message.text
 
     try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
 
-        if os.path.exists(file_name):
-
-            with open(file_name, "rb") as video:
-                bot.send_video(message.chat.id, video)
-
-            os.remove(file_name)
-
-        else:
-            bot.send_message(message.chat.id, "❌ Download failed")
-
-    except Exception as e:
-        bot.send_message(message.chat.id, "❌ Instagram blocked or invalid link")
-        print(e)
-
-
-bot.infinity_polling()
+        # Downloading message
